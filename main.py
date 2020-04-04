caac57b0e6098bd0a6e3594e5d0255eb2ea15cb5
Alex <35878325+Poriseler@users.noreply.github.com>
2020-04-04 20:09:41 +0200

    task 2

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get('/method')
-def method_type():
-    return {"method": "GET"}
+#@app.get('/method')
+#def method_type():
+   # return {"method": "GET"}
 
-@app.delete('/method')
-def method_type():
-    return {"method": "DELETE"}
+#@app.delete('/method')
+#def method_type():
+#    return {"method": "DELETE"}
 
-@app.put('/method')
-def method_type():
-    return {"method": "PUT"}
+#@app.put('/method')
+#def method_type():
+#    return {"method": "PUT"}
 
-@app.post('/method')
-def method_type():
-    return {"method": "POST"}
+#@app.post('/method')
+#def method_type():
+#    return {"method": "POST"}
