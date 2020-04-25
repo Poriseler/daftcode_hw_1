fd73ee0f1183db57f54222cdc47bc73ed5bc3089
Alex <35878325+Poriseler@users.noreply.github.com>
2020-04-25 23:09:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ app.secret_key = "very constatn and random secret, best 64 characters"
 app.sessions = {}
 app.users={"trudnY": "PaC13Nt"}
 
-class Patient(BaseModel):
-    name: str
-    surename: str
+#class Patient(BaseModel):
+ #   name: str
+ #   surename: str
 
 #class Response(BaseModel):
 #    id: int
  #   patient: Patient
 
-@app.get('/patient/{pk}')
-def patient_detail(pk: int):
-    if pk not in dict_of_patients:
-        raise HTTPException(status_code=204, detail="No such patient")
-    return dict_of_patients[pk]
+#@app.get('/patient/{pk}')
+#def patient_detail(pk: int):
+ #   if pk not in dict_of_patients:
+  #      raise HTTPException(status_code=204, detail="No such patient")
+  #  return dict_of_patients[pk]
 
-@app.post("/patient", response_model=Response)
-def receive_patient(pt: Patient):
-    dict_of_patients[app.patients_number] = pt
-    app.patients_number += 1
-    return Response(id=app.patients_number-1, patient=pt)       
+#@app.post("/patient", response_model=Response)
+#def receive_patient(pt: Patient):
+ #   dict_of_patients[app.patients_number] = pt
+ #   app.patients_number += 1
+ #   return Response(id=app.patients_number-1, patient=pt)       
         
 #@app.post("/patient", response_model=Response)
 #def receive_patient(pt: Patient):
