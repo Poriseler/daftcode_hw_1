d14fb98681715eaa786700b6da700048b7778568
Alex <35878325+Poriseler@users.noreply.github.com>
2020-04-27 09:43:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -70,11 +70,14 @@ def method_type():
 # adding some features
 
 @app.post("/patient")
-def receive_patient(response: Response, PatientData: Patient, session_token: str = Depends(is_cookie)):
+def receive_patient(response: Response, PatientData: Patient, session_token: str = Cookie(None)):
     print(app.sessions)
-    if session_token is None:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return "You are not allowed to be here!"
+    
+    
+    
+ #   if session_token is None:
+ #       response.status_code = status.HTTP_401_UNAUTHORIZED
+ #       return "You are not allowed to be here!"
     id = app.patients_number
     app.dict_of_patients[id] = PatientData.dict()
     response.headers["Location"] = f"/patient/{id}"
@@ -83,21 +86,17 @@ def receive_patient(response: Response, PatientData: Patient, session_token: str
 
 
 @app.get("/patient")
-def show_everyone(response: Response, session_token: str = Depends(is_cookie)):
-    print(app.sessions)
-    if session_token is None:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return "You are not allowed to be here!"
+def show_everyone(response: Response, session_token: str = Cookie(None)):
+	if session_token not in app.session_tokens:
+		raise HTTPException(status_code=401, detail="Unathorised")
     response.status_code = status.HTTP_302_FOUND
     return app.dict_of_patients
 
 
 @app.get("/patient/{id}")
-def show_one(id: int, response: Response, session_token: str = Depends(is_cookie)):
-    print(app.sessions)
-    if session_token is None:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return "You are not allowed to be here!"
+def show_one(id: int, response: Response, session_token: str = Cookie(None)):
+	if session_token not in app.session_tokens:
+		raise HTTPException(status_code=401, detail="Unathorised")
     if id in app.dict_of_patients:
         return app.dict_of_patients[id]
     else:
@@ -105,11 +104,9 @@ def show_one(id: int, response: Response, session_token: str = Depends(is_cookie
 
 
 @app.delete("/patient/{id}")
-def kill_him(id: int, response: Response, session_token: str = Depends(is_cookie)):
-    print(app.sessions)
-    if session_token is None:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return "You are not allowed to be here!"
+def kill_him(id: int, response: Response, session_token: str = Cookie(None)):
+	if session_token not in app.session_tokens:
+		raise HTTPException(status_code=401, detail="Unathorised")
     response.status_code = status.HTTP_302_FOUND
     app.dict_of_patients.pop(id, None)
 
