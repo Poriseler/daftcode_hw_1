4a207a029f14620598cdaa171710b862f91fb89e
Alex <35878325+Poriseler@users.noreply.github.com>
2020-05-03 10:30:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -116,6 +116,22 @@ async def update_customer(customer_id: int, request: dict={}):
         app.db_connection.commit()
     return app.db_connection.execute("SELECT * FROM customers WHERE CustomerId = ?",(customer_id,)).fetchone()
 
+@app.get("/sales")
+async def show_numbers(category: str):
+    app.db_connection.row_factory = sqlite3.Row
+
+    if category =="customers":
+        data = app.db_connection.execute("SELECT customers.CustomerId, Email, Phone, round(Total,2) AS Sum FROM customers \
+                                         JOIN invoices ON customers.CustomerId = invoices.CustomerId \
+                                         GROUP BY customers.CustomerId \
+                                         ORDER BY Sum DESC, customers.CustomerId").fetchall()
+
+    else:
+        raise HTTPException(status_code=404, detail={"error": "There is no such category"})
+    return data
+
+
+
 @app.get('/method/')
 def method_type():
     return {"method": "GET"}
